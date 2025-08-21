e38d5d3f9524eb63a5918b50096370bd226a59e9
devam <devamvankhede05@gmail.com>
2025-08-21 15:10:46 +0530

    Use env vars for API URL in blogServices

--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:8000/api/blogs";
+const API_URL = process.env.REACT_APP_API_URL as string;
 
 export async function createBlog(blog: any) {
   return axios.post(API_URL, blog);
