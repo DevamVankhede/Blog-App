413abff01b1be813bde50f26272b028ddd568ded
devam <devamvankhede05@gmail.com>
2025-08-21 15:37:09 +0530

    Use env vars for API URL in blogServices

--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -1,23 +1,30 @@
 import axios from "axios";
 
+// Base API URL from environment
 const API_URL = process.env.REACT_APP_API_URL as string;
+console.log("API_URL is:", API_URL);
 
+// Create a blog
 export async function createBlog(blog: any) {
-  return axios.post(API_URL, blog);
+  return axios.post(`${API_URL}/api/blogs`, blog);
 }
 
+// Fetch all blogs
 export async function fetchBlogs() {
-  return axios.get(API_URL);
+  return axios.get(`${API_URL}/api/blogs`);
 }
 
+// Delete a blog
 export async function deleteBlog(id: string) {
-  return axios.delete(`${API_URL}/${id}`);
+  return axios.delete(`${API_URL}/api/blogs/${id}`);
 }
 
+// Get a blog by ID
 export async function getBlogById(id: string) {
-  return axios.get(`${API_URL}/${id}`);
+  return axios.get(`${API_URL}/api/blogs/${id}`);
 }
 
+// Update a blog
 export async function updateBlog(id: string, blog: any) {
-  return axios.put(`${API_URL}/${id}`, blog);
+  return axios.put(`${API_URL}/api/blogs/${id}`, blog);
 }
